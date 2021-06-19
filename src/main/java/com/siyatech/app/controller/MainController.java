ae3d1235d1845fd22358f82354e0b19a3a71bcf3
Govind Anantwal <govindanantwal@gmail.com>
2021-06-19 22:04:58 +0530

    Project setup 1.0 First Commit

---
+++ src/main/java/com/siyatech/app/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.siyatech.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+	
+	@GetMapping("/greet")
+	public String getGreeting()
+	{
+		return "Hello Docker working";
+	}
+}
